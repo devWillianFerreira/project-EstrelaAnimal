1078fcc28bb3d98f74232d2a1ecb1c50eb9a91a9
zReBolTz <ferreiraw36@gmail.com>
2025-02-25 21:51:06 -0300

    feat: Adicionando Banner na Pagina Home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,17 @@
-import { ShoppingBagIcon } from "lucide-react";
+import { Menu, ShoppingBagIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex justify-between w-full h-15 bg-white items-center px-14 py-10">
-      <div className="flex items-center gap-4">
-        <span>
-          <img src="logo.png" />
-        </span>
-        <h1 className="font-bold text-2xl">Estrela Animal</h1>
+    <div className="flex justify-between w-full max-w-screen h-15 bg-white items-center px-5 lg:px-14 py-10">
+      <div className="text-blue-950 lg:hidden">
+        <Menu />
       </div>
-      <div className="flex flex-row gap-9 text-blue-950">
+      <div className="flex items-center gap-4 ">
+        <img src="logo.png" />
+        <h1 className="font-bold lg:text-2xl">Estrela Animal</h1>
+      </div>
+      <div className=" hidden    lg:flex flex-row gap-9 text-blue-950">
         <Link to="/">
           <h1>Home</h1>
         </Link>
@@ -21,7 +22,7 @@ const Header = () => {
           <h1>Sobre Nós</h1>
         </Link>
       </div>
-      <div className="bg-blue-950 px-2 py-2 rounded-full ">
+      <div className="bg-blue-950 px-2 py-2 rounded-full">
         <Link to="/cart" className="relative text-white ">
           <ShoppingBagIcon />
           <span className="absolute text-white bg-amber-500 rounded-full px-2 py-0.2 flex justify-center items-center -top-6 -right-3">
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
+import BannerHome from "../../components/BannerHome";
+
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <BannerHome />
     </div>
   );
 };
