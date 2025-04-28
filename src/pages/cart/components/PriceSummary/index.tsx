e4385d9d34874032800070be5cc1a9b960bbeb9f
zReBolTz <ferreiraw36@gmail.com>
2025-04-28 19:52:36 -0300

    feat: Componente do valor total do pedido

---
+++ src/pages/cart/components/PriceSummary/index.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+const PriceSummary = () => {
+  return (
+    <div className=" rounded-md p-3 shadow-2xl hover:shadow-1xl transition-shadow duration-300 mt-5">
+      <p className="font-bold text-2xl mt-3">Pedidos</p>
+      <div>
+        <div className="border-2 border-slate-100 w-full mt-5"></div>
+        <div className="flex justify-between items-center mt-5">
+          <p className="font-medium"> SubTotal: </p>
+          <p className="font-medium">R$ 59.00</p>
+        </div>
+        <div className="flex justify-between items-center mt-3">
+          <p className="font-medium">Frete: </p>
+          <p className="text-green-700 font-medium">Grátis</p>
+        </div>
+        <div className="flex justify-between items-center mt-3 gap-3">
+          <p className="text-green-700 font-medium">Cupum: </p>
+          <input
+            type="text"
+            className="border-1 rounded-md border-slate-500 p-1 w-full"
+          />
+        </div>
+        <div className="border-2 border-slate-100 w-full mt-5"></div>
+        <div className="flex justify-between">
+          <p className="font-bold">Total: </p>
+          <p className="font-bold ">R$ 59,00</p>
+        </div>
+      </div>
+      <Link to="/">
+        <h1 className="p-4 bg-blue-950 text-white rounded-md mt-6 font-bold flex justify-center">
+          CHECKOUT
+        </h1>
+      </Link>
+    </div>
+  );
+};
+
+export default PriceSummary;
