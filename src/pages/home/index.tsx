6d1808e2fa34e45f2bb967cb708821580f379bae
zReBolTz <ferreiraw36@gmail.com>
2025-07-26 17:28:02 -0300

    feat: Adicionado Lista de Produtos na página home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,26 @@ import BannerHome from "./components/bannerHome";
 import TrendingProducts from "./components/TrendingProducts";
 import { Link } from "react-router-dom";
 import BannerHome2 from "./components/bannerHome2";
+import { useEffect, useState } from "react";
+import { productsProps } from "../products";
 
 const HomePage = () => {
+  const [products, setProducts] = useState<productsProps[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/produtos?_limit=6")
+      .then((res) => res.json())
+      .then((data) => {
+        const newProduct: productsProps[] = data.map((item: any) => ({
+          id: item.id,
+          category: item.categoria,
+          name: item.nome,
+          image: item.imagem,
+          price: item.preco,
+          type: item.tipo,
+        }));
+        setProducts(newProduct);
+      });
+  }, []);
   return (
     <div className="overflow-hidden">
       <BannerHome />
@@ -41,14 +59,34 @@ const HomePage = () => {
           <CardCategories image="cate-4.jpg" name="Passáro" />
         </div>
       </div>
-      <div className="container mx-auto px-4 flex flex-col">
+      <div className="container mx-auto px-4 flex flex-col w-full">
         <div className="w-full  text-blue-950 flex flex-col  lg:justify-between items-center">
           <h1 className="text-2xl font-bold">Produtos em Altas</h1>
           <Link to="/products">
-            <h2 className="font-semibold ">Ver todos</h2>
+            <h2 className="font-semibold underline">Ver todos</h2>
           </Link>
         </div>
-        <TrendingProducts />
+        <div className="w-full flex flex-col mt-5 items-center lg:flex-row justify-between mb-10 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 flex-1 ">
+            {products.map((product) => (
+              <Link to={`/ProductDetail/${product.id}`}>
+                <TrendingProducts
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                  key={product.id}
+                />
+              </Link>
+            ))}
+          </div>
+          <div className=" hidden lg:block  ">
+            <img
+              src="banner-8.jpg"
+              alt="Produtos em Altas"
+              className="rounded-md w-full relative "
+            />
+          </div>
+        </div>
       </div>
       <BannerHome2 />
     </div>
