e22a0d21f41a24d51ab2c871d521c2b53869bb3d
zReBolTz <ferreiraw36@gmail.com>
2025-07-30 20:02:11 -0300

    feat: Melhorando a responsividade do cardCategories

--- src/pages/home/components/cardCatetories/index.tsx
+++ src/pages/home/components/cardCatetories/index.tsx
@@ -9,9 +9,11 @@ const CardCategories = ({ image, name }: CardCategoriesProps) => {
   return (
     <section>
       <Link to="/products">
-        <div className="w-[250px] h-[340px] bg-white rounded-md mb-10 flex flex-col justify-center items-center gap-6 hover:scale-110 duration-300 cursor-pointer">
-          <img src={image} className="w-[170px] rounded-full" />
-          <h1 className="font-bold text-blue-950 font text-2xl">{name}</h1>
+        <div className="w-full max-w-sm h-full max-h-80 bg-white rounded-md mb-10 flex flex-col justify-center items-center gap-6 hover:scale-110 duration-300 cursor-pointer ">
+          <img src={image} className="w-full max-w-40 rounded-full p-2" />
+          <h1 className="font-bold text-blue-950  text-sm lg:text-2xl">
+            {name}
+          </h1>
         </div>
       </Link>
     </section>
